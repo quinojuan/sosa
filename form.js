69035a41323b918eb8ee6a926650b89cc6107036
Juan Quino <quinojuan@gmail.com>
2023-04-05 23:16:24 -0300

    formulario

--- form.js
+++ form.js
@@ -1,1 +1,11 @@
-var input = document.getElementsByClassName("forulario__input")
+var inputs = document.getElementsByClassName("formulario__input");
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("keyup", () => {
+    if (this.value.length >= 1) {
+      this.nextElementSibling.classList.add("fijar");
+    } else {
+      this.nextElementSibling.classList.remove("fijar");
+    }
+  });
+}
